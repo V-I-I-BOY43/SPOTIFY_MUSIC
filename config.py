6bc7f3524ff0c12f5002da592391527b5e784bff
V-I-I-BOY43 <147950486+V-I-I-BOY43@users.noreply.github.com>
2024-11-05 05:29:31 +0530

    Update config.py
    Signed-off-by: V-I-I-BOY43 <147950486+V-I-I-BOY43@users.noreply.github.com>

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/SPOTIFY_MUSIC",
+    "https://github.com/V-I-I-BOY43/SPOTIFY_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -67,10 +67,10 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BABY09_WORLD")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+OL6jdTL7JAJjYzVl")
-SOURCE = getenv("SOURCE", "https://github.com/BABY-MUSIC/BABYXMUSIC")
-CHAT = getenv("CHAT", "https://t.me/chattinghuboo")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/quizbys")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/quizbysu")
+SOURCE = getenv("SOURCE", "https://github.com/V-I-I-BOY43/SPOTIFY_MUSIC")
+CHAT = getenv("CHAT", "https://t.me/unique_boy11")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
